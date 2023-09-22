36d468cfcc4f5ef698091d3b264aac62b8872bdb
Avanthika M <130764101+Avanthika0@users.noreply.github.com>
2023-09-22 11:31:14 +0530

    Initial commit

---
+++ src/screens/Desktop/Desktop.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "./style.css";
+
+export const Desktop = () => {
+  return (
+    <div className="desktop">
+      <div className="div">
+        <p className="our-work-pricing">
+          Our
+          Work&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Pricing&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Unlimited
+          Graphic Design
+          101&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          Blog&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Contact
+        </p>
+        <img className="image" alt="Image" src="https://c.animaapp.com/H1baYC5Y/img/image-1.png" />
+        <p className="an-affordable">
+          <span className="text-wrapper">An </span>
+          <span className="span">Affordable</span>
+          <span className="text-wrapper"> Unlimited Graphic Design Service</span>
+        </p>
+        <div className="overlap">
+          <p className="p">Unlimited graphic design, at an affordable price</p>
+          <p className="text-wrapper-2">
+            With the help of our infinite small graphic design service, we will pair you with a competent designer that
+            will handle all of your small graphic design requirements.
+          </p>
+        </div>
+        <p className="text-wrapper-3">All at an affordable flat price. No interviews. No fuss. No worries</p>
+        <div className="overlap-group">
+          <div className="rectangle" />
+          <div className="text-wrapper-4">Get Started</div>
+          <div className="text-wrapper-5">How it works</div>
+        </div>
+        <div className="text-wrapper-6">DA</div>
+      </div>
+    </div>
+  );
+};
